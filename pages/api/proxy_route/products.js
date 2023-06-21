3d73699f6fac3e145fd33653f47c1c6624672c14
steven9404 <steven.ye.hinterlands@gmail.com>
2023-06-22 00:59:25 +0800

    products proxy

--- pages/api/proxy_route/products.js
+++ pages/api/proxy_route/products.js
@@ -7,7 +7,29 @@ const handler = async (req, res) => {
   const { client } = await clientProvider.offline.graphqlClient({
     shop: req.user_shop,
   });
-  res.status(200).send({ content: "Products Be Working" });
+  const response = await client.query({
+    data: `{
+        shop {
+            products(first: 50) {
+                edges {
+                    node {
+                        id
+                        title
+                        images(first: 1) {
+                            edges {
+                                node {
+                                    src
+                                    altText
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }`,
+  });
+  res.status(200).send(JSON.stringify(response));
 };
 
 export default withMiddleware("verifyProxy")(handler);
